838227595c25ca72c6667e8fd23801f17902940d
Kirill Pavlov <roodboss1992@gmail.com>
2026-04-27 21:05:25 +0300

    Initial commit: Unity project setup

---
+++ Assets/Scripts/Game/Player/Bullet.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{   
+    [SerializeField]
+    private GameObject _bulletPrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
